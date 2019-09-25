5fbdc8dca3c41e6d649902bf7ccffd74effc4dd9
Tierra <sr.undershaft@gmail.com>
2019-09-25 13:31:55 -0500

    debug db

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 const mysql = require("mysql")
 
-let PORT = process.env.PORT || 8080;
-
-
 //Create MySQL connection
 
 const conn;
+
 if (process.env.JAWSDB_URL) {
     conn = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    conn = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'burgers_db'
-    });
-};
+// } else {
+//     conn = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'password',
+//         database: 'burgers_db'
+//     });
+// };
 conn.connect(function (err, data) {
     if (err) throw err;
     console.log('Connected as id:' + conn.threadId)
