885c5f14dd4da4f5c0f84fe512ed290dbe796737
Tierra <sr.undershaft@gmail.com>
2019-09-25 13:16:04 -0500

    setup jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,20 @@ let PORT = process.env.PORT || 8080;
 
 //Create MySQL connection
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-});
-
-conn.connect(function(err, data){
-    if(err) throw err;
-    console.log('Connected as id:' +conn.threadId)
+const conn;
+if (process.env.JAWSDB_URL) {
+    conn = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    });
+};
+conn.connect(function (err, data) {
+    if (err) throw err;
+    console.log('Connected as id:' + conn.threadId)
 });
 
 module.exports = conn;
